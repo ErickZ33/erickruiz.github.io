41e92f6e59a991c9b36bef5ed7ae932dcf555650
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-09-26 10:50:41 -0500

    added new home features

--- js/script.js
+++ js/script.js
@@ -13,29 +13,6 @@ $(document).ready(function () {
         fullscreen();
     });
 
-    
-    var words = ["building", "solving", "innovating"],
-        el = document.getElementById('magic'),
-        word_counter = 0,
-        character_counter = 0;
-    function updateText() {
-        if (words[word_counter][character_counter] == ' ') {
-            el.innerHTML = el.innerHTML + '&nbsp';
-            character_counter++;
-        } else {
-            el.innerHTML = el.innerHTML + words[word_counter][character_counter++];
-        }
-        if (character_counter == words[word_counter].length + 1) {
-            word_counter++; 	//choose a different word
-            character_counter = 0;	//start over with the first character of the word
-            el.innerHTML = '';  //set the html to be blank
-            //if we're displaying the last word, go back to the first word
-            if (word_counter == words.length)
-                word_counter = 0;
-        }
-    }
-    setInterval(updateText, 250);
-
     $("#scrollToHome").click(function () {
         $('html, body').animate({
             scrollTop: $("#hero").offset().top
@@ -60,23 +37,23 @@ $(document).ready(function () {
         }, 2000);
     });
 
-    $(window).scroll(function() {
-        $('#aboutMeHeader img').each(function(){
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function () {
+        $('#aboutMeHeader img').each(function () {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+400) {
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
                 $(this).addClass("slideRight");
             }
         });
     });
 
-    $(window).scroll(function() {
-        $('.aboutMeText').each(function(){
-        var imagePos = $(this).offset().top;
+    $(window).scroll(function () {
+        $('.aboutMeText').each(function () {
+            var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+400) {
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 400) {
                 $(this).addClass("slideLeft");
             }
         });
